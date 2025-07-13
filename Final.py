42986f2abd5acafb5bef28798091e0c360d3109f
apari1 <apari@pucp.edu.pe>
2025-07-12 23:00:27 -0500

    Update Final.py

--- Final.py
+++ Final.py
@@ -46,8 +46,8 @@ if pagina_seleccionada == '🏠 Inicio':
         # Paso 10: Mostrar el gráfico
         st.plotly_chart(fig1)
         # Paso 11: Agregar interpretación del gráfico
-        st.write:{"La mayoría de los usuarios encuestados para la base de datos fueron del género femenino, lo que demuestra su relevancia dentro del público objetivo. Este dato sugiere una mayor participación en ciertos hábitos de consumo musical, preferencias de contenido de la plataforma y comportamientos digitales más activos que otros grupos.
-"}
+        st.markdown(f"<div style='text-align: justify; font-size: 18px;'>{La mayoría de los usuarios encuestados para la base de datos fueron del género femenino, lo que demuestra su relevancia dentro del público objetivo. Este dato sugiere una mayor participación en ciertos hábitos de consumo musical, preferencias de contenido de la plataforma y comportamientos digitales más activos que otros grupos.
+}</div>", unsafe_allow_html=True)
     
     with col2:
         # Paso 10: Contar momentos de escucha de cada usuario y asignar columnas
